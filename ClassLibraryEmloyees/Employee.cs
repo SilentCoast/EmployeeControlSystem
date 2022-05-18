add73c582b90d1fa72d767eabbb9e29de97e953f
MishaKS <75131984+MishaKS@users.noreply.github.com>
2022-05-18 08:30:04 +0600

    add ClassLibraryEmloyees add class Employee in there

---
+++ ClassLibraryEmloyees/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryEmloyees
+{
+    public class Employee
+    {
+        public string FIO { get; set; } 
+        public string Sex { get; set; }
+        public string BirthData { get; set; } 
+    }
+
+   
+}
--- PersonalUchet/MainWindow.xaml.cs
+++ PersonalUchet/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ClassLibraryEmloyees;
 
 namespace PersonalUchet
 {
@@ -23,6 +24,7 @@ namespace PersonalUchet
         public MainWindow()
         {
             InitializeComponent();
+            
         }
     }
 }
